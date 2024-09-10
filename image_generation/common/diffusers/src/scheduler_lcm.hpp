0ddfd43d6d2adb53df7551cb5b7cec3c35399fa9
Ilya Lavrenov <sandye64@gmail.com>
2024-09-10 20:58:12 +0400

    EMULATED AND IT WORKS

--- image_generation/common/diffusers/src/scheduler_lcm.hpp
+++ image_generation/common/diffusers/src/scheduler_lcm.hpp
@@ -59,7 +59,7 @@ private:
     std::vector<int64_t> m_timesteps;
 
     uint32_t m_seed;
-    std::mt19937 m_gen;
+    std::vector<std::mt19937> m_gen;
     std::normal_distribution<float> m_normal;
 
     std::vector<float> threshold_sample(const std::vector<float>& flat_sample);
